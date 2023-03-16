471d08905c0f025614557d5a32c2c8d35c5702e0
Igor Bodalov <igorbodalov@gmail.com>
2023-03-16 15:33:11 +0300

    Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая   
+// 1. На вход принимает два числа и выдаёт, 
+// 2. какое число большее,
+// 3. а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+int numberA = 0;
+int numberB = 1;
+{
+double a, b;
+
+        Console.Write("Введите первое число: ");
+        a = Double.Parse(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        b = Double.Parse(Console.ReadLine());
+        if (a > b)
+            Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", a, b);
+        else Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", b, a);
+}
